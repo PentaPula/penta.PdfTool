9ccddd91da8f99ecdee6f4b5d1e9894d019b52cb
igord <igor.dumancic@penta.hr>
2023-07-14 13:19:58 +0200

    Add project files.

---
+++ penta.PdfTool.Shared/Dto/InvoiceItemDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penta.PdfTool.Shared.Dto;
+
+public record InvoiceItemDto(Int32 rbr, ItemDto Item, Decimal UnitCost, Decimal Qty, Decimal Amount);
+   
---
+++ penta.PdfTool.Shared/Dto/ItemDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penta.PdfTool.Shared.Dto;
+
+public record ItemDto(Int32 Id, String Name, String Description);
